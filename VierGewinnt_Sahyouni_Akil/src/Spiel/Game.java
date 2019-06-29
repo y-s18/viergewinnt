04c3ebea99c1f926e0d06f409ad648290dc325f9
yamen <yamen@LAPTOP-SC7TMCN8>
2019-06-29 21:51:48 +0200

    abstracte Klasse Game mit der Attribute implementiert, Methode swapPlayer implementiert

--- VierGewinnt_Sahyouni_Akil/src/Spiel/Game.java
+++ VierGewinnt_Sahyouni_Akil/src/Spiel/Game.java
@@ -1,5 +1,24 @@
 package Spiel;
 
+import Spiel.Token.Color;
+
 public abstract class Game {
+	private Bord bord;
+	private Player redPlayer = new ConsolePlayer(Color.RED, bord);
+	private Player yellowPlayer = new ConsolePlayer(Color.YELLOW, bord);
+	private Player currentPlayer;
+	
+	public Game(Bord bord) {
+		this.bord = bord;
+	}
+	
+	protected void swapPlayer() {
+		if(currentPlayer==redPlayer) {
+			currentPlayer = yellowPlayer;
+		}else {
+			currentPlayer = redPlayer;
+		}
+	}
+
 
 }
