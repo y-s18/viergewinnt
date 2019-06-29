17b0516f33a33b172dfcb4b4b76c7dfc0988b045
yamen <yamen@LAPTOP-SC7TMCN8>
2019-06-29 18:29:40 +0200

    Methode doDrop() und doTurn() implementiert Konstruktor Player() erstellt

--- VierGewinnt_Sahyouni_Akil/src/Spiel/Player.java
+++ VierGewinnt_Sahyouni_Akil/src/Spiel/Player.java
@@ -1,5 +1,26 @@
 package Spiel;
-
+import Spiel.Token.Color;
+import exceptions.*;
 public abstract class Player {
-
+	private Color color;
+	private Bord bord;
+	
+	public Player(Color color, Bord bord) {
+		this.color = color;
+		this.bord = bord;
+	}
+	
+	protected void doDrop(int columnIndex) throws ColumnFullException, IllegalMoveException {
+		if(this.bord.canDrop(columnIndex) == false) {
+			throw new ColumnFullException("Column Full!");
+		}
+		
+		if(this.color == Color.RED){
+			this.bord.dropToken(this.bord.getToken(Color.RED), columnIndex);
+		}if(this.color == Color.YELLOW) {
+			this.bord.dropToken(this.bord.getToken(Color.YELLOW), columnIndex);
+		}
+	}
+	
+	public abstract void doTurn();
 }
