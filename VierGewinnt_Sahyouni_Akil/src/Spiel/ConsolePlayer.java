2625c9c2fbd3788d013b72ba4e8d3ef635523fcd
yamen <yamen@LAPTOP-SC7TMCN8>
2019-06-29 22:53:23 +0200

    Methode doTurn() implementiert, Exceptions hinzugefuegt

--- VierGewinnt_Sahyouni_Akil/src/Spiel/ConsolePlayer.java
+++ VierGewinnt_Sahyouni_Akil/src/Spiel/ConsolePlayer.java
@@ -12,19 +12,29 @@ public class ConsolePlayer extends Player {
 	public void doTurn() {
 		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 		String input = "";
+		int inputInt = 0;
 		try {
 			input = consoleReader.readLine();
+			System.out.println();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		int inputInt = Integer.parseInt(input);
 		try {
-			super.doDrop(inputInt-1);
+			inputInt = Integer.parseInt(input);
+			if(inputInt >0 && inputInt <8) {
+				super.doDrop(inputInt-1);
+			}else {
+				System.out.println("Invaild Input!");
+				System.out.println();
+			}
+		}catch(NumberFormatException e) {
+			System.out.println("Invalid Input, Please try again!");
 		} catch (ColumnFullException e) {
 			e.printStackTrace();
 		} catch (IllegalMoveException e) {
 			e.printStackTrace();
 		}
+
+		}
 	}
-}
+
