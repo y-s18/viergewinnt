ca491b017913f44e2fe5656abe921cf968ce964c
yamen <yamen@LAPTOP-SC7TMCN8.mshome.net>
2019-06-30 19:37:26 +0200

    Methode doGame mit do-While statt For-Schleife implementiert, allgemeine Anpassungen

--- VierGewinnt_Sahyouni_Akil/src/Spiel/ConsoleGame.java
+++ VierGewinnt_Sahyouni_Akil/src/Spiel/ConsoleGame.java
@@ -1,14 +1,43 @@
 package Spiel;
-
+import Spiel.Token.Color;
+/*
+ * yamen.sahyouni@tu-clausthal.de 		/ mohamad.deyaa.akil@tu-clausthal.de
+ * Vorname: Yamen  						/ Deyaa
+ * Nachname: Sahyouni  					/ Akil
+ * Matrikel-Nr.498773  					/ 498618
+ *
+ */
+/**
+ * 
+ * @author Yamen Sahyouni, Deyaa Akil
+ * 
+ * Diese Klasse repraesentiert eine Konsole von Game und ist eine Unterklasse von der Klasse Game
+ *
+ */
 public class ConsoleGame extends Game {
 	
 	public ConsoleGame(Bord bord) {
 		super(bord);
 	}
-	
+	/**
+	 * Diese Methode waehlt aus, einen Spieler zu beginnen. 
+	 * Dann fragt die Methode solange abwechselnd die Spieler nach einem Zug Mithilfe der Methode (doTurn),
+	 * bis es einen Sieger oder ein Unentschieden in der Spielpartie gibt.
+	 * Nach jedem Zug wird das Spielbrett auf der Konsole ausgegeben.
+	 * 
+	 * @param player1, braucht ein Objekt vom Typ Player
+	 * @param player2, braucht ein Objekt vom Typ Player
+	 */
 	public void doGame(Player player1, Player player2) {
-		this.setRedPlayer(player1);
-		this.setYellowPlayer(player2);
+		if(player1.getColor() == Color.RED) {
+			this.setRedPlayer(player1);
+		}else{
+			this.setYellowPlayer(player1);
+		}if(player2.getColor() == Color.RED) {
+			this.setRedPlayer(player2);
+		}else{
+			this.setYellowPlayer(player2);
+		}
 		int randomBeginn = java.util.concurrent.ThreadLocalRandom.current().nextInt(1,3);
 		switch(randomBeginn) {
 		case 1:
@@ -18,33 +47,45 @@ public class ConsoleGame extends Game {
 			this.setCurrentPlayer(player2);
 			break;
 		}
-		for(int i = 0;i<=41;i++) {
+		this.getBord().toString();
+		do {
 			if(this.getCurrentPlayer() == this.getRedPlayer()) {
 				System.out.println("Spieler X, was ist dein nächster Zug?");
 				System.out.print("Eingabe: ");
+				int prevNumOfRedTokens = this.getBord().countRedTokens();
 				this.getRedPlayer().doTurn();
-				
-				this.swapPlayer();
-				this.getBord().toString();
+				int currNumOfRedTokens = this.getBord().countRedTokens();
+				if(currNumOfRedTokens != prevNumOfRedTokens) {
+					this.swapPlayer();
+					this.getBord().toString();
+				}else {
+					continue;
+				}
 				if(this.getBord().testVictory() == Winner.RED 
 						|| this.getBord().testVictory() == Winner.TIE) {
 					System.out.println("Winner is: " + this.getBord().testVictory());
 					break;
 				}
-				
-			}else {
+			}if(this.getCurrentPlayer() == this.getYellowPlayer()) {
 				System.out.println("Spieler O, was ist dein nächster Zug?");
 				System.out.print("Eingabe: ");
+				int prevNumOfYellowTokens = this.getBord().countYellowTokens();
 				this.getYellowPlayer().doTurn();
-				
-				this.swapPlayer();
-				this.getBord().toString();
+				int currNumOfYellowTokens = this.getBord().countYellowTokens();
+				if(currNumOfYellowTokens !=prevNumOfYellowTokens) {
+					this.swapPlayer();
+					this.getBord().toString();
+				}else {
+					continue;
+				}
 				if(this.getBord().testVictory() == Winner.YELLOW 
 						|| this.getBord().testVictory() == Winner.TIE) {
 					System.out.println("Winner is: " + this.getBord().testVictory());
 					break;
 				}
 			}
-		}
+		}while(this.getBord().testVictory() != Winner.YELLOW
+				&& this.getBord().testVictory() != Winner.RED
+				&& this.getBord().testVictory() != Winner.TIE);
 	}
 }
--- VierGewinnt_Sahyouni_Akil/src/Spiel/ConsolePlayer.java
+++ VierGewinnt_Sahyouni_Akil/src/Spiel/ConsolePlayer.java
@@ -2,13 +2,29 @@ package Spiel;
 import java.io.*;
 import exceptions.*;
 import Spiel.Token.Color;
+/*
+ * yamen.sahyouni@tu-clausthal.de 		/ mohamad.deyaa.akil@tu-clausthal.de
+ * Vorname: Yamen  						/ Deyaa
+ * Nachname: Sahyouni  					/ Akil
+ * Matrikel-Nr.498773  					/ 498618
+ *
+ */
+/**
+ * 
+ * @author Yamen Sahyouni, Deyaa Akil
+ * 
+ * Diese Klasse repraesentiert eine Konsole von Spieler und ist eine Unterklasse von der Klasse Player
+ *
+ */
 public class ConsolePlayer extends Player {
 	
 	
 	public ConsolePlayer(Color color, Bord bord) {
 		super(color, bord);
 	}
-	
+	/**
+	 * Diese Methode fragt den Nutzer ueber Konsole nach einer Eingabe
+	 */
 	public void doTurn() {
 		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 		String input = "";
@@ -21,20 +37,22 @@ public class ConsolePlayer extends Player {
 		}
 		try {
 			inputInt = Integer.parseInt(input);
-			if(inputInt >0 && inputInt <8) {
-				super.doDrop(inputInt-1);
+			if(inputInt >=0 && inputInt <7) {
+				super.doDrop(inputInt);
 			}else {
 				System.out.println("Invaild Input!");
 				System.out.println();
 			}
 		}catch(NumberFormatException e) {
 			System.out.println("Invalid Input, Please try again!");
-		} catch (ColumnFullException e) {
-			e.printStackTrace();
-		} catch (IllegalMoveException e) {
-			e.printStackTrace();
-		}
-
+			System.out.println();
+		}catch (ColumnFullException e) {
+			System.out.println("Column Full!");
+			System.out.println();
+		}catch (IllegalMoveException e) {
+			System.out.println("Illegal Move!");
+			System.out.println();
 		}
 	}
+}
 
